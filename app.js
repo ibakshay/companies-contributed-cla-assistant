737267d137d522815fcf9d251e4fb3b3ecbbae4f
Thomas Jansen <t.jansen@sap.com>
2019-04-26 13:50:53 +0200

    simplify app.js and remove node_modules from git

--- app.js
+++ app.js
@@ -1,92 +1,29 @@
-const express = require('express');
-const _ = require('lodash');
-const request = require('request-promise');
-const Promise = require('bluebird');
-const app = express();
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening on port ${port}`));
+const express = require('express')
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit({'user-agent': 'companies-contributed-cla-app'/*, auth: `token <token>`*/})
 
-var companyNames = [];
-var userUrls = [];
+const app = express()
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on port ${port}`))
 
-/*Initial API call for this  App server*/
+// register new express route to return companies contributed to cla-assistant
 app.get('/companies-contributed-cla', (req, res) => {
-  var contributors = {
-    url: 'https://api.github.com/repos/cla-assistant/cla-assistant/contributors',
-    method: 'GET',
-    headers: {
-      'user-agent': 'companies-contributed-cla-App'
-    }
-  };
 
-  /* first external API call from this App server for getting all the contributors details */
-  request(contributors).then(function(response) {
-    var responseArr = JSON.parse(response);
-    /*for getting all the users  profile endpoints  from the response and storing them in an array (userUrls) with header information*/
-    getUsersUrl(responseArr);
-  }).catch(function(err) {
-    console.log('The API call to get all the contributors failed', err);
-  }).then(function() {
-    /* API calls to all the user profile endpoints to get the company information
-      for each contributor by grouping it by promises using Promise.map provided by the bluebird module .*/
-    Promise.map(userUrls, function(userUrl) {
-      return request(userUrl).then(function(response) { 
-        var responseObj = JSON.parse(response);
-        /*for getting  all the company names from response  and storing them  in an Array (companyNames) */
-        getCompanyNames(responseObj);
-      });
-    }).catch(function(err) {
-      console.log('The API call  to user profile for getting company details is failed', err);
-    }).then(function() {
-      /*for counting the number of contributors in each company and sorting based on most contributors at the top*/
-      var sortedCompanyInfo = getSortedCompanyList(companyNames);
+  // get all contributors (ignore pagination for now)
+  octokit.repos.listContributors({owner: 'cla-assistant', repo: 'cla-assistant'}).then(({ data, status, headers }) => {
+      
+    // get details for each user
+    const users = data.map(user => { return {login: user.login, url: user.url} })
+    Promise.all(users.map(user => octokit.users.getByUsername({username: user.login}))).then((data) => {
+      
+      const contributors = data.map(response => { return { login: response.data.login, company: response.data.company} })
+      const companies = [...new Set(contributors.map( contributor => contributor.company ))]
 
-      /* the final response (output)  is a list with companies   and number of contributors to each company is displayed in JSON format
-        and is also sorted in such a way that the company with most number  of contributors will be displayed on the top */
-      res.send(JSON.stringify(sortedCompanyInfo));
-    });
-
-  });
-
-
-});
-
-function getUsersUrl(responseArr) {
-  responseArr.forEach((item, index, array) => {
-    var urlObj = {
-      url: item.url,
-      method: 'GET',
-      headers: {
-        'user-agent': 'companies-contributed-cla-App'
-      }
-    };
-    userUrls.push(urlObj);
-  });
-
-  return userUrls;
-}
-
-function getCompanyNames(responseObj) {
-  if (responseObj.company === null) {
-    responseObj.company = 'Unknown';
-  }
-  companyNames.push(responseObj);
-}
-
-function getSortedCompanyList(companyNames) {
-  /*for counting the frequency / occurrences of  companyNames array elements and returns a single
-   object with  company names(keys)  and their  respective number of occurrences(values)*/
-  var companyInfo = _.countBy(companyNames, 'company');
-  /* for  sorting the result of _.countBy (companyInfo) in descending order and storing in an array (sortedCompanyInfo)*/
-  var sortedCompanyInfo = _.chain(companyInfo). //
-  map(function(currentValue, index) {
-      return {
-        company: index,
-        contributors: currentValue
-      }
-    }).sortBy('contributors')
-    .reverse()
-    .value();
-  return sortedCompanyInfo;
-
-}
+      // get all company contributors and order list descending 
+      let companyContributors = []
+      companies.map(company => companyContributors.push({company: company, contributors: contributors.filter(contributor => contributor.company === company).length}))
+      companyContributors = companyContributors.sort((first, second) => { return first.contributors <= second.contributors ? 1 : -1})
+      res.send(JSON.stringify(companyContributors, 2))
+    })
+  })
+})
